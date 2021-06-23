2d67069afb9e72b17b10aeafd74fde44dfcd5972
McFunjy <barminov_01@mail.ru>
2021-06-23 15:57:19 +0300

    Add passed_test, seed, and methods for user and test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(title)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+        .where(categories: { title: title })
+        .order('tests.title DESC')
+        .pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def passed_tests_by_level(level)
+    Test.joins('JOIN passed_tests ON tests.id = passed_tests.test_id')
+        .where('passed_tests.user_id = :user_id AND level = :level', user_id: id, level: level)
+  end
 end
