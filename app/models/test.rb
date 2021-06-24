9129fe7e5a399ce7ca8bea8c1e76ad8b1b8f4f49
McFunjy <barminov_01@mail.ru>
2021-06-24 19:53:33 +0300

    Add option dependent, empty strings between belongs_to and has_many and refactored seeds

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,11 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :questions
-  has_many :passed_tests
+
+  has_many :questions, dependent: :destroy
+  has_many :passed_tests, dependent: :destroy
   has_many :users, through: :passed_tests
+
   def self.tests_by_category(title)
     joins(:category)
       .where(categories: { title: title })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :passed_tests
+  has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests
-  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
+
   def passed_tests_by_level(level)
     tests.where(level: level)
   end
