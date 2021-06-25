d25ec70abc6e42994cc4db79f667c56c423a4e44
McFunjy <barminov_01@mail.ru>
2021-06-25 20:48:10 +0300

    Fix validation for columncategory_id and returned null option

--- db/migrate/20210622204522_create_passed_tests.rb
+++ db/migrate/20210622204522_create_passed_tests.rb
@@ -1,8 +1,8 @@
 class CreatePassedTests < ActiveRecord::Migration[6.1]
   def change
     create_table :passed_tests do |t|
-      t.references :test, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :test, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
