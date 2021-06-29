10d4ee229054d3e3281193af987bd163e6f70ff0
McFunjy <barminov_01@mail.ru>
2021-06-29 20:31:05 +0300

    Add validates and scope-methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,18 @@ class Test < ApplicationRecord
   has_many :passed_tests, dependent: :destroy
   has_many :users, through: :passed_tests
 
-  def self.tests_by_category(title)
-    joins(:category)
-      .where(categories: { title: title })
-      .order(title: :desc)
-      .pluck(:title)
+  validates :title, presence: true, uniquencess { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_category, -> (title) { joins(:category)
+  .where(categories: { title: title })
+  .order(title: :desc)
+}
+
+  def self.titles_by_category(title)
+    by_category(title).pluck(:title)
   end
 end
